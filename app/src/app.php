9417d2998684992a2e7469659fda2501cf6038f3
CristianMendoza001 <saintseyya94@hotmail.com>
2023-04-22 02:41:05 -0600

    Agregado de entities
    Se agregan las entities y el controller, pero no funciona el extend del base controller

--- app/src/app.php
+++ app/src/app.php
@@ -9,6 +9,9 @@ $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
 $routes->add('blog', new Routing\Route('/blog', [
     '_controller' => 'App\Controller\BlogApiController::index',
 ]));
+$routes->add('product', new Routing\Route('/product', [
+    '_controller' => 'App\Controller\ProductController::index',
+]));
 
 return $routes;
 

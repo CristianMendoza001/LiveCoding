609e8435d9bcebe7e340008b47bf52e1ce51be3a
CristianMendoza001 <saintseyya94@hotmail.com>
2023-04-22 05:02:50 -0600

    Controllers For Blogs (Finished) and for Comments

---
+++ app/src/Controller/BlogsController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Blogs;
+
+class BlogsController extends AbstractController
+{
+
+    #[Route('/blog', name: 'app_blogs')]
+    public function index(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $blogs = [];
+        $msg = '';
+        $date = new \DateTime();
+
+        if($request->isMethod('get')){
+            $blogs_db = $entityManager->getRepository(Blogs::class)->where('deleted_at', NULL)->findAll();
+            foreach($blogs_db as $blog) {
+                $blogs[] = [
+                    'id' => $blog->getId(),
+                    'title' => $blog->getTitle(),
+                    'content' => $blog->getContent(),
+                    'author' => $blog->getAuthor(),
+                    'slug' => $blog->getSlug()
+                ];
+            }
+            $msg = 'Watching all blogs';
+        }//end if it's a get
+        else{
+            $title = $request->request->get('title');
+            $content = $request->request->get('content');
+            $author = $request->request->get('author');
+            
+            if($title == NULL || $content == NULL || $author == NULL){
+                $msg = 'Error in given data';
+            }
+            else {
+                $slug = strtolower(str_replace(' ', '_', $title));
+                $blog = new Blogs();
+                $blog->setCreatedAt($date);
+                $blog->setUpdatedAt($date);
+                $blog->setTitle($title);
+                $blog->setContent($content);
+                $blog->setAuthor($author);
+                $blog->setSlug($slug);
+                
+                $entityManager->persist($blog);
+                $entityManager->flush();
+
+                $msg = 'Blog created Succesfully';
+            }
+        }//end else it's a get
+        return new JsonResponse(['message' => $msg, 'data' => $blogs]);
+    }
+
+    #[Route('/blog_id', name: 'blog_id')]
+    public function getBlogBySlug(Request $request, EntityManagerInterface $entityManager, string $slugBlog): JsonResponse
+    {
+        $blog = [];
+        $msg = '';
+        if($idBlog == NULL){
+            $msg = 'The slug of the blog is needed';
+            return new JsonResponse(['message' => $msg, 'blog' => $blog]);
+        }
+
+        $blog_db = $entityManager->getRepository(Blogs::class)->where('deleted_at', NULL)->where('slug', $slugBlog)->find();
+        foreach($blog_db as $blog) {
+            $blog = [
+                'id' => $blog->getId(),
+                'title' => $blog->getTitle(),
+                'content' => $blog->getContent(),
+                'author' => $blog->getAuthor(),
+                'slug' => $blog->getSlug()
+            ];
+        }
+        $msg = 'Blog find it';
+
+        if($request->isMethod('get'))
+            return new JsonResponse(['message' => $msg, 'blog' => $blog]);
+        else {
+            $entityManager->remove($blog_db[0]);
+            $entityManager->flush();
+            return new JsonResponse(['message' => $msg.' and deleted it!', 'blog' => $blog]);
+        }
+    }      
+
+}
--- app/src/app.php
+++ app/src/app.php
@@ -6,14 +6,21 @@ $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
     'year' => null,
     '_controller' => 'App\Controller\LeapYearController::index',
 ]));
-$routes->add('blog', new Routing\Route('/blog', [
-    '_controller' => 'App\Controller\BlogApiController::index',
-]));
+
+//TODO: Delete these routes for testing only
 $routes->add('product', new Routing\Route('/product', [
     '_controller' => 'App\Controller\ProductController::index',
 ]));
 $routes->add('create_product', new Routing\Route('/create_product', [
     '_controller' => 'App\Controller\ProductController::createProduct',
+]));
+
+//Blog section
+$routes->add('blog', new Routing\Route('/blog', [
+    '_controller' => 'App\Controller\BlogsController::index',
+]));
+$routes->add('blog_id', new Routing\Route('/blog/{slug}', [
+    '_controller' => 'App\Controller\BlogsController::getBlogBySlug',
 ]));
 
 return $routes;
